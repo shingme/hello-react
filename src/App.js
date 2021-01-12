92d180df8a4ee870af4aba696dc87c30e7c2d8c2
shingme <sinmi1620@gmail.com>
2021-01-12 23:04:06 +0900

    함수형 컴포넌트 선언 및 설정 1) 모듈 내보내기(export defalut 컴포넌트) 2) 모듈 불러오기(import 컴포넌트명 from '컴포넌트위치') 3) props 컴포넌트 속성 설정(children, 비구조화 할당, propTypes(props검증, isRequired) 등

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import MyComponent from './MyComponent'; /* MyComponent 컴포넌트 불러오기 */
+import MyComponent from './MyComponent'; // MyComponent 컴포넌트 불러오기
 
 const App = () => {
-  return <MyComponent/>;
+  return <MyComponent name="React" favoriteNumber={3}>리액트</MyComponent>; /* props 렌더링 : props값은 해당 컴포넌트를 불러와 사용하는 부모 컴포넌트에서 설정 */
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types'; /* propTypes(컴포넌트의 필수 props를 지정하거나 props의 타입을 지정)를 사용하기 위한 임포트 */
+//함수형 컴포넌트
+const MyComponent = ({name, favoriteNumber, children}) => {
+  //const {name,children} = props; /* 비구조화 할당 : 객체에서 값을 추출하는 문법(구조 분해 문법) */
+  return (
+    <div>안녕하세요, 제 이름은 {name}입니다. <br/> {/* 부모 컴포넌트에서 설정한 name값이 대응됨, 위에서 파라미터 형식으로 지정하지 않았으면 props.name 으로 사용해야함 */}
+    children 값은 {children}입니다. <br/> {/* 컴포넌트 태그 사이의 내용을 보여주는 props */}
+    제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+    </div>
+  );
+};
 
-const MyComponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>
+MyComponent.defaultProps = { /* 부모 컴포넌트에서 props 값을 따로 지정하지 않았을 때 보여줄 기본값 설정 */
+  name : '기본 이름'
+};
+
+MyComponent.propTypes = { /* props 타입을 지정 */
+  name : PropTypes.string, /* name 값은 무조건 String 형태로 전달 */
+  favoriteNumber : PropTypes.number.isRequired // isRequired : 필수 props로 지정 (해당 props에 값 전달안하면 콘솔창에 경고메세지가 뜸)
 };
 
 export default MyComponent; /* 모듈 내보내기 */
